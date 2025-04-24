fb828ea37069cc660361f9d4e30a4067b101020b
øbook <obooklage@free.fr>
2025-04-24 21:27:44 +0200

    Switch to uv python packages manager.

--- ping_plotengine.py
+++ ping_plotengine.py
@@ -15,54 +15,55 @@ import numpy as np
 import time
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
-from packaging import version # check plotext version
+from packaging import version  # check plotext version
 
 host = "google.com"
-maxdata = 50 # 50 horizontal values max
+maxdata = 50  # 50 horizontal values max
 
 colors = {"green":"\33[1;32;40m ", "yellow":"\033[1;33;40m ", "red":"\033[1;31;40m ", "purple":"\033[1;35;40m ", "white":"\033[1;37;40m ", "reset":"\33[0m"}
 default_color = colors["green"]
 
+
 def IsWindows():
-    return(platform.system().lower() == "windows")
+    return platform.system().lower() == "windows"
+
 
 def PingTime(host):
-
     """
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
     """
 
     # Option for the number of packets as a function of
-    if IsWindows(): # Windows
+    if IsWindows():  # Windows
         param = '-n'
-    else: # Linux
+    else:  # Linux
         param = '-c'
 
     # Building the command. Ex: "ping -c 1 google.com"
     command = ['ping', param, '2', host]
-        
-    if IsWindows():
-        CREATE_NO_WINDOW = 0x08000000 # Nouveau
-        p = subprocess.run(command, shell=False, stdout=subprocess.PIPE, creationflags=CREATE_NO_WINDOW) # Windows
-    else: 
-        p = subprocess.run(command, shell=False, stdout=subprocess.PIPE) # Linux
 
-    #Windows
     if IsWindows():
-        result = p.stdout.decode('cp1252') # .encode('utf-8') # encodage OEM 850
-        result = result.replace("\r\n","\n")
+        CREATE_NO_WINDOW = 0x08000000  # Nouveau
+        p = subprocess.run(command, shell=False, stdout=subprocess.PIPE, creationflags=CREATE_NO_WINDOW)  # Windows
+    else: 
+        p = subprocess.run(command, shell=False, stdout=subprocess.PIPE)  # Linux
+
+    # Windows
+    if IsWindows():
+        result = p.stdout.decode('cp1252')  # .encode('utf-8') # encodage OEM 850
+        result = result.replace("\r\n", "\n")
         tab1 = result.split("\n")
-        
+
         for i in tab1:
             if i.find("TTL") != -1:
                 tab2 = i.split(" ")
                 for j in tab2:
-                        if j.find("temps") != -1: #Change to your language here !
-                            k = j.split("=")
-                            time = k[1];
-                            return float(time)
-    #Linux
+                    if j.find("temps") != -1:  # Change to your language here !
+                        k = j.split("=")
+                        time = k[1]
+                        return float(time)
+    # Linux
     else:
         tab1 = p.stdout.decode('utf-8').split('\n')
         for i in tab1:
@@ -70,31 +71,37 @@ def PingTime(host):
                 j = i.split("=")
                 k = j[1].split("/")
                 return float(k[1])
-
     return -1
+
 
 def PlotPing(host, list, maxdata):
     ping = PingTime(host)
     list = np.insert(list, 0, ping)
     list = np.delete(list, maxdata)
-    plt.clp()
+    if version.parse(plt.__version__) > version.parse("5.3.2"):
+        plt.clear_color()
+    else:
+        plt.clear_figure()   # removed ?  plt.clear_plot()
     plt.clt()
     # plt.scatter(list, label = "Ping", color = "gold", fillx = True)
-    plt.scatter(list, fillx = True)
-    if(ping < 30 ):
+    plt.scatter(list, fillx=True)
+    if ping < 30:
         color = colors["green"]
-    elif ping < 80 :
+    elif ping < 80:
         color = colors["yellow"]
     else:
         color = colors["red"]
-    plt.title(default_color  + "ICMP reponse from " + host + color + str(ping) + default_color + "ms")
-    if version.parse(plt.__version__) > version.parse("2.3.1"):  # in version 3.0.1, nocolor is changed to colorless()
+    plt.title(default_color + "ICMP reponse from " + host + color + str(ping) + default_color + "ms")
+    if version.parse(plt.__version__) >= version.parse("5.3.2"):
+        plt.clear_color()
+    elif version.parse(plt.__version__) > version.parse("2.3.1"):  # in version 3.0.1, nocolor is changed to colorless()
         plt.colorless()
     else:
         plt.nocolor()
     plt.sleep(0.01)
     plt.show()
-    return(list)
+    return list
+
 
 if __name__ == "__main__":
     pingslist = np.zeros(shape=(maxdata))
